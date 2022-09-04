2ec207f52c5d1e2deb42c227f15cab5d03aca511
Poptum Charoennaew <3c14irlmao@gmail.com>
2022-09-04 21:53:55 +0700

    Add db related funcs

---
+++ config/db.go
@@ -0,0 +1,26 @@
+package config
+
+import (
+	"context"
+	"log"
+	"os"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func DBConnect() *mongo.Client {
+	uri := os.Getenv("MONGODB_URI")
+	if uri == "" {
+		log.Fatal("You must set your 'MONGODB_URI' environmental variable. See\n\t https://www.mongodb.com/docs/drivers/go/current/usage-examples/#environment-variable")
+	}
+	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(uri))
+	if err != nil {
+		panic(err)
+	}
+	return client
+}
+
+func GetCollection(client *mongo.Client, collectionName string) *mongo.Collection {
+	return client.Database("Cluster0").Collection(collectionName)
+}
